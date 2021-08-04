60b0f0e0d3dc420c020151fc63bd7a141cd17d0f
solehee <wlsgml3839@naver.com>
2021-08-04 15:03:42 +0900

    update: DBconnector import 수정

--- Library_System/src/model/OverdueCountDao.java
+++ Library_System/src/model/OverdueCountDao.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import db.DBConnector;
 
 public class OverdueCountDao {
 	private Connection conn;
