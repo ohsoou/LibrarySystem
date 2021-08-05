4b3041c09a6f8cd71c80b2d66d55b6765d6ca779
최진호 <@192.168.219.100>
2021-08-05 14:22:10 +0900

    BookDao update

--- Library_System/src/model/BookDao.java
+++ Library_System/src/model/BookDao.java
@@ -11,34 +11,95 @@ import java.util.ArrayList;
 import db.DBConnector;
 
 public class BookDao {
-	ArrayList<BookDto> overdueCountList = new ArrayList<BookDto>();
-	
-	public static void main(String[] args) {
-		
-		String sql = "SELECT * FROM BOOK";
-		
-		try {
+	private Connection conn;
+	PreparedStatement pstmt;
+	int deleteRows;
+	int updateRows;
+	int Rows;
+	String sql;
+
+	public ArrayList<BookDto> list() {		
+	ArrayList<BookDto> BookDao = new ArrayList<BookDto>();		
+	String sql = "SELECT * FROM book";
+
+	try (
 			Connection conn 
 				= DBConnector.getConnection();		
 			PreparedStatement pstmt
 				= conn.prepareStatement(sql);
 			ResultSet rs 
 			= pstmt.executeQuery();
-			
-			while(rs.next()) {
-				int BOOK_ID = rs.getInt("BOOK_ID");
-				String ISBN = rs.getString("ISBN");
-				String LOAN_STATE = rs.getString("LOAN_STATE");		
-			}
-			
-			rs.close();
-			pstmt.close();
-			conn.close();
-			
+			){	
+		 while(rs.next()) {
+				int book_id = rs.getInt("book_id");
+				int ISBN = rs.getInt("ISBN");
+				String loan_state = rs.getString("loan_state");		
+
+				BookDto BookDto = new BookDto(book_id, ISBN, loan_state);
+				BookDao.add(BookDto);
+			}		
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		return BookDao;	
+
+	}
+
+	public int insertBook(int book_id, int ISBN, String loan_state) {
+		sql = "INSERT INTO book VALUES(?,?,?)";
+		Rows = 0;
 		
-		System.out.println("main ³¡");
-}
-}
+		try (
+			Connection conn 
+				= DBConnector.getConnection();		
+			PreparedStatement pstmt
+				= conn.prepareStatement(sql);			
+		){
+			pstmt.setInt(1, book_id);
+			pstmt.setInt(2, ISBN);
+			pstmt.setString(3, loan_state);
+
+			Rows = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		return Rows;
+	}
+
+	public int updateBook(int book_id, int ISBN) {
+		sql = "UPDATE book SET" + book_id + "= ? WHERE ISBN = ?";
+		updateRows = 0;
+		
+		try {
+			pstmt.setInt(1, book_id);
+			pstmt.setInt(2, ISBN);		
+
+			updateRows = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return updateRows;
+	}
+
+	public int deleteBook(int book_id) {
+		sql = "DELETE FROM book WHERE book_id = ?";
+		deleteRows = 0;
+		
+		try(
+			Connection conn 
+				= DBConnector.getConnection();		
+			PreparedStatement pstmt
+				= conn.prepareStatement(sql);	
+		){
+			pstmt.setInt(1, book_id);
+			
+			deleteRows = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return deleteRows;
+	}
+
+} 
