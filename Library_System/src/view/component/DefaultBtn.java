fa9ce1db6d2643db920c5fc2da2fda6f62b38f3a
ohsoou <eoghkqks@naver.com>
2021-08-05 21:54:59 +0900

    create defaultBtn

--- Library_System/src/view/component/DefaultBtn.java
+++ Library_System/src/view/component/DefaultBtn.java
@@ -1,12 +1,25 @@
 package view.component;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JButton;
 
+@SuppressWarnings("serial")
 public class DefaultBtn extends JButton{
-	public DefaultBtn() {
-		setPreferredSize(new Dimension(100,40));
+	public DefaultBtn(String text) {
+		this(text, 100, 40);
+	}
+	public DefaultBtn(String text, int width, int height) {
+		super(text);
+		setPreferredSize(new Dimension(width,height));
+		
+		setBackground(new Color(255, 95, 46)); 
+		setForeground(new Color(255, 255, 255));
+		setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 14));
+		setBorderPainted(false); 
 	}
 
 }
