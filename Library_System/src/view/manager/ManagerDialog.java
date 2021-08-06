4824bdc5fe7fb87402e79692d6ea47c974a26680
ohsoou <eoghkqks@naver.com>
2021-08-06 19:35:45 +0900

    change arrangement method

--- Library_System/src/view/manager/ManagerDialog.java
+++ Library_System/src/view/manager/ManagerDialog.java
@@ -10,6 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class ManagerDialog extends JDialog{
 	public ManagerDialog(JFrame frame, String title) {
@@ -26,26 +27,37 @@ public class ManagerDialog extends JDialog{
 		JLabel pageTitle = new JLabel(title);
 		pageTitle.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
 		
-		constraints.gridx = 0;
-        constraints.gridy = 0;     
+		constraints.gridwidth = GridBagConstraints.REMAINDER;    
         add(pageTitle, constraints);
         
-        constraints.gridy = 1;     
+        constraints.gridwidth = GridBagConstraints.RELATIVE; 
         add(new FormPanel(), constraints);
  
-        constraints.gridx = 1;
-        constraints.gridy = 1;     
+        constraints.gridwidth = GridBagConstraints.REMAINDER;  
         add(new FilePanel(), constraints);
         
-        constraints.gridwidth = 1;
-        constraints.gridx = 0;
-        constraints.gridy = 2; 
-        add(new JButton("CANCEL"), constraints);
-        constraints.gridx = 1;
-        add(new JButton("SUBMIT"), constraints);
+//        constraints.gridwidth = 1;
+//        constraints.gridx = 0;
+//        constraints.gridy = 2; 
+//        add(new JButton("CANCEL"), constraints);
+//        constraints.gridx = 1;
+//        add(new JButton("SUBMIT"), constraints);
+        
+
 
 		setSize(730, 530);
 	
+	}
+	
+	class ButtonPanel extends JPanel {
+		public ButtonPanel() {
+			setBackground(new Color(244,240,240));
+			JButton cancel = new JButton("CANCEL");
+			JButton submit = new JButton("SUBMIT");
+			
+			add(cancel);
+			add(submit);
+		}
 	}
 
 }
