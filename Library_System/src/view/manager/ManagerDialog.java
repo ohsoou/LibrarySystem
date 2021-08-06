2003b2544c58edb52182d9e0e08df73b3d4d774b
ohsoou <eoghkqks@naver.com>
2021-08-06 20:22:57 +0900

    arrange managerDialog

--- Library_System/src/view/manager/ManagerDialog.java
+++ Library_System/src/view/manager/ManagerDialog.java
@@ -1,10 +1,12 @@
 package view.manager;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.GridLayout;
+
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -17,33 +19,22 @@ public class ManagerDialog extends JDialog{
 		super(frame, title);
 		this.setLocation(100, 100);
 		getContentPane().setBackground(new Color(244,240,240));
-		setLayout(new GridBagLayout());
 		
-		GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor = GridBagConstraints.WEST;
-        constraints.insets = new Insets(10, 10, 10, 10);
-        constraints.fill = GridBagConstraints.BOTH;
-		
+		setLayout(new BorderLayout());
 		JLabel pageTitle = new JLabel(title);
 		pageTitle.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
 		
-		constraints.gridwidth = GridBagConstraints.REMAINDER;    
-        add(pageTitle, constraints);
-        
-        constraints.gridwidth = GridBagConstraints.RELATIVE; 
-        add(new FormPanel(), constraints);
- 
-        constraints.gridwidth = GridBagConstraints.REMAINDER;  
-        add(new FilePanel(), constraints);
-        
-//        constraints.gridwidth = 1;
-//        constraints.gridx = 0;
-//        constraints.gridy = 2; 
-//        add(new JButton("CANCEL"), constraints);
-//        constraints.gridx = 1;
-//        add(new JButton("SUBMIT"), constraints);
-        
-
+		add(pageTitle, "North");
+		
+		JPanel center = new JPanel();
+		center.setBackground(new Color(244,240,240));
+		center.setLayout(new FlowLayout());
+		center.add(new FormPanel());
+		center.add(new FilePanel());
+		add(center, "Center");
+		
+		JPanel bottom = new ButtonPanel();
+		add(bottom, "South");
 
 		setSize(730, 530);
 	
@@ -51,12 +42,23 @@ public class ManagerDialog extends JDialog{
 	
 	class ButtonPanel extends JPanel {
 		public ButtonPanel() {
-			setBackground(new Color(244,240,240));
+			setPreferredSize(new Dimension(730, 100));
+			setLayout(new GridLayout(1, 2));
+			setBackground(Color.green);
 			JButton cancel = new JButton("CANCEL");
 			JButton submit = new JButton("SUBMIT");
 			
+			cancel.setBackground(new Color(244,240,240)); 
+			cancel.setForeground(new Color(158, 158, 158));
+			cancel.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 22));
+			
+			submit.setBackground(new Color(244,240,240)); 
+			submit.setForeground(new Color(158, 158, 158));
+			submit.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 22));
+
 			add(cancel);
 			add(submit);
+			
 		}
 	}
 
