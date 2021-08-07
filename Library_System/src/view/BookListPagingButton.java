0c546c49dcd4c64ab88772348dc77b21a7c80a95
최진호 <@192.168.219.100>
2021-08-08 03:03:38 +0900

    TablePanel & BookListPagingButton & BookListTable 작업

---
+++ Library_System/src/view/BookListPagingButton.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+
+public class  BookListPagingButton extends JButton{
+	private JButton DefaultButton;
+	private ButtonGroup ButtonGroup;	
+	private JToggleButton ToggleButton;
+	private TablePanel TablePanel = new TablePanel();
+	
+	public BookListPagingButton (String text) {
+		DefaultButton = new JButton(text);
+		DefaultButton.setBorderPainted(false); 
+		TablePanel.add(DefaultButton);
+	}
+	
+	public void JToggleButton(String text) {	
+		ToggleButton = new JToggleButton(text);
+		ToggleButton.setBorderPainted(false); 
+		TablePanel.add(ToggleButton);
+		
+		ButtonGroup.add(ToggleButton);	
+	}
+	
+}
---
+++ Library_System/src/view/TablePanel.java
@@ -0,0 +1,23 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TablePanel extends JPanel{
+	private JPanel contentPane;
+	
+	public TablePanel() {	
+		contentPane = new JPanel();
+		
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+	}
+
+	private void setContentPane(JPanel contentPane2) {
+
+		
+	}
+
+}
