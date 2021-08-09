ce71f91f385f078a2ba2316d10a76d6f70765aee
ohsoou <eoghkqks@naver.com>
2021-08-09 20:51:28 +0900

    solve conflict

--- Library_System/src/view/DefaultFrame.java
+++ Library_System/src/view/DefaultFrame.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package view;
 
 import java.awt.Color;
@@ -8,42 +7,12 @@ import java.awt.Point;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-public class DefaultFrame extends JFrame{
-
-	public DefaultFrame() {
-		super();
-		
-		getContentPane().setBackground(new Color(225, 238, 246));
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocation(new Point(100, 100));
-		setSize(new Dimension(980, 600));
-		setVisible(true);
-	}
-	
-	public static void main(String[] args) {
-		new DefaultFrame();
-	}
-}
-=======
-package view;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Point;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-import view.advertising.AdCenterPanel;
-import view.advertising.AdTopPanel;
-import view.advertising.AdvertisingPanel;
 import view.manager.ManagerPanel;
 
 public class DefaultFrame extends JFrame{
 
 	public DefaultFrame() {
-		super("ë„ì„œê´€ ì‹œìŠ¤í…œ");
+		super("µµ¼­°ü ÇÁ·Î±×·¥");
 		
 		JPanel search = new ManagerPanel();
 		
@@ -63,4 +32,4 @@ public class DefaultFrame extends JFrame{
 		new DefaultFrame();
 	}
 }
->>>>>>> dev
+
