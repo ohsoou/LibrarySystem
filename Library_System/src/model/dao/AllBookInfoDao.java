56a1302b826e44b1546da9d87aa95fab5d912fbb
ohsoou <eoghkqks@naver.com>
2021-08-09 21:27:28 +0900

    update: add select option

--- Library_System/src/model/dao/AllBookInfoDao.java
+++ Library_System/src/model/dao/AllBookInfoDao.java
@@ -53,6 +53,104 @@ public class AllBookInfoDao {
 		return allBookInfoList;
 	}
 	
+	public ArrayList<AllBookInfo> listByPublisher(String publisher){
+		allBookInfoList = new ArrayList<>();
+		String sql = "SELECT * FROM allBookInfo WHERE publisher LIKE '%?%'";
+		try (
+				Connection	conn = DBConnector.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+		   ) {
+			pstmt.setString(1, publisher);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				AllBookInfo bookInfo = new AllBookInfo(
+						rs.getInt("Book_id"),
+						rs.getLong("ISBN"),
+						rs.getString("loan_state"),
+						rs.getString("KDC"),
+						rs.getString("category_name"),
+						rs.getString("author"),
+						rs.getString("publisher"),
+						rs.getDate("publication_date"),
+						rs.getString("book_name"),
+						rs.getString("imagepath"),
+						rs.getString("summary")
+						
+						);
+				allBookInfoList.add(bookInfo);
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allBookInfoList;
+	}
+	
+	public ArrayList<AllBookInfo> listByBookName(String name){
+		allBookInfoList = new ArrayList<>();
+		String sql = "SELECT * FROM allBookInfo WHERE book_name LIKE '%?%'";
+		try (
+				Connection	conn = DBConnector.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+		   ) {
+			pstmt.setString(1, name);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				AllBookInfo bookInfo = new AllBookInfo(
+						rs.getInt("Book_id"),
+						rs.getLong("ISBN"),
+						rs.getString("loan_state"),
+						rs.getString("KDC"),
+						rs.getString("category_name"),
+						rs.getString("author"),
+						rs.getString("publisher"),
+						rs.getDate("publication_date"),
+						rs.getString("book_name"),
+						rs.getString("imagepath"),
+						rs.getString("summary")
+						
+						);
+				allBookInfoList.add(bookInfo);
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allBookInfoList;
+	}
+	public ArrayList<AllBookInfo> listByAuthor(String author){
+		allBookInfoList = new ArrayList<>();
+		String sql = "SELECT * FROM allBookInfo WHERE author LIKE '%?%'";
+		try (
+				Connection	conn = DBConnector.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+		   ) {
+			pstmt.setString(1, author);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				AllBookInfo bookInfo = new AllBookInfo(
+						rs.getInt("Book_id"),
+						rs.getLong("ISBN"),
+						rs.getString("loan_state"),
+						rs.getString("KDC"),
+						rs.getString("category_name"),
+						rs.getString("author"),
+						rs.getString("publisher"),
+						rs.getDate("publication_date"),
+						rs.getString("book_name"),
+						rs.getString("imagepath"),
+						rs.getString("summary")
+						
+						);
+				allBookInfoList.add(bookInfo);
+				rs.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return allBookInfoList;
+	}
+	
 	public ArrayList<AllBookInfo> listNewBook(){
 		allBookInfoList = new ArrayList<>();
 		String sql = "SELECT * FROM allBookInfo ORDER BY PUBLICATION_DATE DESC";
