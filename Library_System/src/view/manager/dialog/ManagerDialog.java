73538155bae706783ff86c7a43e169d13c522007
ohsoou <eoghkqks@naver.com>
2021-08-09 14:44:48 +0900

    enhancement: add summary form and change design

--- Library_System/src/view/manager/dialog/ManagerDialog.java
+++ Library_System/src/view/manager/dialog/ManagerDialog.java
@@ -2,6 +2,7 @@ package view.manager.dialog;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 
@@ -9,33 +10,55 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+
+import view.component.DefaultPanel;
+import view.component.RoundTextField;
 
 public class ManagerDialog extends JDialog{
 	public ManagerDialog(JFrame frame, String title) {
 		super(frame, title);
 		this.setLocation(100, 100);
 		getContentPane().setBackground(new Color(244,240,240));
-		
 		setLayout(new BorderLayout());
+		
+		// top (title)
 		JLabel pageTitle = new JLabel(title);
-		pageTitle.setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.BOLD, 20));
+		Border border = pageTitle.getBorder();
+	    Border margin = new EmptyBorder(20,50,0,0);
+	    pageTitle.setBorder(new CompoundBorder(border, margin));
+		pageTitle.setFont(new Font("∏º¿∫ ∞ÌµÒ", Font.BOLD, 30));
 		
 		add(pageTitle, "North");
 		
-		JPanel center = new JPanel();
-		center.setBackground(new Color(244,240,240));
+		// center(form)
+		JPanel center = new DefaultPanel(new Color(244,240,240));
 		center.setLayout(new FlowLayout());
 		center.add(new FormPanel());
 		center.add(new FilePanel());
+		
+		JPanel summary = new DefaultPanel(new Color(244,240,240));
+		JLabel summaryLabel = new JLabel("¡Ÿ∞≈∏Æ");
+		border = summaryLabel.getBorder();
+	    margin = new EmptyBorder(0,0,0,20);
+	    summaryLabel.setBorder(new CompoundBorder(border, margin));
+        JTextField summaryField = new RoundTextField(550, 50);
+		
+        summary.add(summaryLabel);
+		summary.add(summaryField);
+		center.add(summary);
 		add(center, "Center");
 		
+		// bottom (button)
 		JPanel bottom = new ButtonPanel();
 		add(bottom, "South");
 
-		setSize(730, 530);
+		setSize(730, 580);
 	
 	}
-	
-	
 
 }
