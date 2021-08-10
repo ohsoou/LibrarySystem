274e6a7dfac6e48f08e3fc8cf09429cecddf35ac
1103-22 <1103-22@host.docker.internal>
2021-08-10 20:50:41 +0900

    LoginPage 수정

---
+++ Library_System/src/view/login/LoginErrorLabel.java
@@ -0,0 +1,13 @@
+package view.login;
+
+import java.awt.Font;
+
+import javax.swing.JLabel;
+
+public class LoginErrorLabel extends JLabel{
+
+		public LoginErrorLabel() {
+			setHorizontalAlignment(CENTER);
+			setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 13));
+		}
+}
