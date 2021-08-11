5bcbcab078f652919a420f4b1bcad1efa76c58a7
ohsoou <eoghkqks@naver.com>
2021-08-11 16:28:39 +0900

    update: refacor table,search panel
    separate DefaultSelectedTablePanel to search, DefaultTable

--- Library_System/src/view/manager/ManagerPanel.java
+++ Library_System/src/view/manager/ManagerPanel.java
@@ -1,22 +1,96 @@
 package view.manager;
 
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import model.dao.AllBookInfoDao;
+import model.dto.AllBookInfo;
 import view.component.DefaultPanel;
-import view.component.DefaultSearchedTablePanel;
+import view.component.DefaultTablePanel;
 
 public class ManagerPanel extends DefaultPanel{
+	private DefaultTablePanel tablePanel;
+	private JTable table;
+	private AllBookInfo selectedBook;
+	
+	private SearchPanel searchPanel;
+	private JButton searchButton;
 	
 	public ManagerPanel() {
 		super();
 		
-		JPanel title = new TitlePanel();
-		JPanel table = new DefaultSearchedTablePanel();
+		JPanel titlePanel = new TitlePanel();
 		
-		add(title);
-	  	add(table);
+		searchPanel = new SearchPanel();
+		searchButton = searchPanel.getSearchButton();
+		searchButton.addActionListener(new searchListener());
+		
+		tablePanel = new DefaultTablePanel();
+		table = tablePanel.getTable();
+		table.getSelectionModel().addListSelectionListener(new selectTableRowListener());
+		
+		
+		add(titlePanel);
+		add(searchPanel);
+		add(tablePanel);
+	 
 	
 	}
 	
+	public AllBookInfo getSelectedBook() {
+		return selectedBook;
+	}
+
+	private class selectTableRowListener implements ListSelectionListener {
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			if (!e.getValueIsAdjusting() && table.getSelectedRow() != -1) {
+				ArrayList<AllBookInfo> booklist = tablePanel.getBooklist();
+				int startIndex = tablePanel.getStartIndex();
+				selectedBook = booklist.get(startIndex + table.getSelectedRow());
+				System.out.println(selectedBook);
+			}
+
+		}
+	}
+	
+	private class searchListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			ArrayList<AllBookInfo> booklist = tablePanel.getBooklist();
+			booklist.clear();
+			tablePanel.setBooklist(booklist);
+
+			int category = searchPanel.getBookCategory().getSelectedIndex();
+			String text = searchPanel.getSearchBar().getText();
+			
+			AllBookInfoDao bookinfodao = AllBookInfoDao.getInstance();
+			
+			switch (category) {
+			case 1: // 책이름
+				booklist = bookinfodao.listByBookName(text);
+				break;
+			case 2: // 저자
+				booklist = bookinfodao.listByAuthor(text);
+				break;
+			case 3: // 출판사
+				booklist = bookinfodao.listByPublisher(text);
+				break;
+			default: // 전체
+				booklist = bookinfodao.listBySomethig(text);
+				break;
+			} 
+			tablePanel.setBooklist(booklist);
+			tablePanel.getFirstPageButton().doClick();
+		}
+		
+	}
 
 }
--- Library_System/src/view/manager/dialog/OpenDialogListener.java
+++ Library_System/src/view/manager/dialog/OpenDialogListener.java
@@ -7,12 +7,19 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 
+import view.manager.ManagerPanel;
+
 public class OpenDialogListener implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton btn = (JButton)(e.getSource());
 		
 		JFrame df = (JFrame)btn.getRootPane().getParent();
+		
+		if(btn.getText().equals("추가")) {
+			ManagerPanel parentPanel = (ManagerPanel)btn.getParent().getParent();
+			
+		}
 		JDialog update = new ManagerDialog(df, "도서 " + btn.getText());
 		update.setVisible(true);
 		
