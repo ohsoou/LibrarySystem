c20aedf425ae3c7a1e38f79e7d728b386cbc06d4
solehee <wlsgml3839@naver.com>
2021-08-12 19:25:37 +0900

    update:Package명 변경, DB연결, table 하이라이트

---
+++ Library_System/src/view/returns/ReturnBtn.java
@@ -0,0 +1,12 @@
+package view.returns;
+import view.component.DefaultButton;
+
+public class ReturnBtn extends DefaultButton{
+
+	public ReturnBtn(String text) {
+		super(text);
+		
+	}
+
+}
+   
---
+++ Library_System/src/view/returns/ReturnTitleLable.java
@@ -0,0 +1,20 @@
+package view.returns;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
+
+public class ReturnTitleLable extends JLabel {
+	public ReturnTitleLable(String text) {
+		
+		super(text,JLabel.CENTER);
+		
+		setFont(new Font("¸¼Àº °íµñ",Font.BOLD | Font.PLAIN,52));
+		setForeground(new Color(0, 78, 102));
+		setBorder(new EmptyBorder(40,0,20,0));
+	} 
+}
+
+ 
