f2883ff6aec490c4c15c45af1aac2ef72b98f380
ohsoou <eoghkqks@naver.com>
2021-08-18 16:34:25 +0900

    change maximumPoolSize

--- Library_System/src/db/DBConnector.java
+++ Library_System/src/db/DBConnector.java
@@ -17,7 +17,7 @@ public class DBConnector {
 	private static Properties prop;
 
 	static {
-		Properties prop = new Properties();
+		prop = new Properties();
 		try {
 			Reader rd = new FileReader("./src/db/db.properties");
 			prop.load(rd);
@@ -34,7 +34,7 @@ public class DBConnector {
 		config.setJdbcUrl(prop.getProperty("url"));
 		config.setUsername(prop.getProperty("user"));
 		config.setPassword(prop.getProperty("password"));
-		config.addDataSourceProperty("maximumPoolSize", 2);
+		config.addDataSourceProperty("maximumPoolSize", 3);
 		config.addDataSourceProperty("cachePrepStmts", "true");
 		config.addDataSourceProperty("prepStmtCacheSize", "250");
 		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
