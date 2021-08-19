56e0674bc831f725284df455f4c3ba25361d6311
ohsoou <eoghkqks@naver.com>
2021-08-19 19:17:02 +0900

    change to static method

--- Library_System/src/view/login/LoginHost.java
+++ Library_System/src/view/login/LoginHost.java
@@ -8,15 +8,15 @@ public class LoginHost {
 		return student_password;
 	}
  
-	public void setStudent_password(String student_password) {
-		this.student_password = student_password;
+	public static void setStudent_password(String student_password) {
+		LoginHost.student_password = student_password;
 	}
 
 	public static String getStudent_num() {
 		return student_num;
 	}
 
-	public void setStudent_num(String student_num) {
-		this.student_num = student_num;
+	public static void setStudent_num(String student_num) {
+		LoginHost.student_num = student_num;
 	}
 }
