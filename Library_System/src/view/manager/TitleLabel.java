68f4ab4b0610ca1a6029d776817b077e16028b51
ohsoou <eoghkqks@naver.com>
2021-08-20 17:38:22 +0900

    change title font

--- Library_System/src/view/manager/TitleLabel.java
+++ Library_System/src/view/manager/TitleLabel.java
@@ -10,7 +10,7 @@ public class TitleLabel extends JLabel{
 	public TitleLabel(String text) {
 		
 		super(text);
-		setFont(new Font("±Ã¼­Ã¼", Font.BOLD, 52));
+		setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 52));
 		setForeground(new Color(0, 78, 102));
 		setBounds(20, 0, 200, 120);
 	}
