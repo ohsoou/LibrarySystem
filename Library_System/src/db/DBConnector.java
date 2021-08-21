9ca0589676977d8027836ea480833c250a173c0e
ohsoou <eoghkqks@naver.com>
2021-08-21 19:11:36 +0900

    update maxLifetime

--- Library_System/src/db/DBConnector.java
+++ Library_System/src/db/DBConnector.java
@@ -34,11 +34,11 @@ public class DBConnector {
 		config.setJdbcUrl(prop.getProperty("url"));
 		config.setUsername(prop.getProperty("user"));
 		config.setPassword(prop.getProperty("password"));
-		config.addDataSourceProperty("maximumPoolSize", 3);
+		config.addDataSourceProperty("maximumPoolSize", 2);
 		config.addDataSourceProperty("cachePrepStmts", "true");
 		config.addDataSourceProperty("prepStmtCacheSize", "250");
 		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-		config.addDataSourceProperty("maxLifetime", "180000");
+		config.addDataSourceProperty("maxLifetime", "700000");
 		
 		datasource = new HikariDataSource(config);
 	}
